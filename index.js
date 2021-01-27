eddf8418ecf68ab72c52602a21f5e0d997c63655
alvaro alves <alvaroalves87@gmail.com>
2021-01-27 00:13:00 -0300

    alteração index

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const app = express()
 app.set("view engine", "ejs")
 
 app.get("/",(req,res) =>{
+    var nome = "Alvaro Alves"
+    var lang = "Javascript"
     //Não precisa colocar o caminho inteiro, ele ja busca direto na pasta views
-    res.render("index")
+    res.render("index",{
+        nome: nome,
+        lang: lang
+    })
 })
 
 app.listen(8080, console.log("app rodando"))
